45f01a504d4504d145341a39fab27148423eca9c
DimaGrach <dima.grach333@gmail.com>
2022-07-21 21:46:32 +0300

    change character service

--- Assets/Scripts/GameScene/Characters/CharacterService.cs
+++ Assets/Scripts/GameScene/Characters/CharacterService.cs
@@ -1,37 +1,96 @@
+using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 namespace GameScene.Characters
 {
     public class CharacterService 
     {
-        private readonly Dictionary<CharacterScreenPositionEnum, CharacterUiView> _characterUiViewMap = new();
-
         public CharacterService(Transform uiCharacter)
         {
             foreach (var characterUi in uiCharacter.GetComponentsInChildren<CharacterUiView>())
             {
+                _dissolveRoutineMap.Add(characterUi.ScreenPosition, null);
                 _characterUiViewMap.Add(characterUi.ScreenPosition, characterUi);
+                _enabledMap.Add(characterUi.ScreenPosition, true);
             }
         }
 
+        private readonly Dictionary<CharacterScreenPositionEnum, CharacterUiView> _characterUiViewMap = new();
+        private readonly Dictionary<CharacterScreenPositionEnum, Coroutine> _dissolveRoutineMap = new();
+        private readonly Dictionary<CharacterScreenPositionEnum, bool> _enabledMap = new();
+
         public void ShowCharacter(CharacterScreenPositionEnum screenPosition, Sprite image)
         {
+            if(image == null) return;
+            
             HideAllCharacters();
-            _characterUiViewMap[screenPosition].Image = image;
+            
+            if(image != _characterUiViewMap[screenPosition].Image.sprite)
+            {
+                HideCharacter(screenPosition, false);
+                DissolveIn(screenPosition);
+            }
+            
+            _characterUiViewMap[screenPosition].Sprite = image;
+            _characterUiViewMap[screenPosition].Visible = true;
+            _enabledMap[screenPosition] = true;
         }
         
-        public void HideCharacter(CharacterScreenPositionEnum screenPosition)
+        public void HideCharacter(CharacterScreenPositionEnum screenPosition, bool isDissolve)
         {
-            _characterUiViewMap[screenPosition].Visible = false;
+            if(_dissolveRoutineMap[screenPosition] != null)
+                CoroutineHelper.Inst.StopCoroutine(_dissolveRoutineMap[screenPosition]);
+            
+            if (isDissolve)
+            {
+                if (_enabledMap[screenPosition])
+                    _dissolveRoutineMap[screenPosition] =
+                        CoroutineHelper.Inst.StartCoroutine(DissolveOut(screenPosition));
+            }
+            else
+            {
+                _characterUiViewMap[screenPosition].Image.sprite = null;
+                _characterUiViewMap[screenPosition].Visible = false;
+                _enabledMap[screenPosition] = false;
+            }
         }
         
         public void HideAllCharacters()
         {
-            foreach (var character in _characterUiViewMap.Values)
+            foreach (var coroutine in _dissolveRoutineMap.Values)
             {
-                character.Visible = false;
+                if(coroutine != null)
+                    CoroutineHelper.Inst.StopCoroutine(coroutine);
             }
+            
+            foreach (var character in _characterUiViewMap)
+            {
+                if (_enabledMap[character.Key])
+                    _dissolveRoutineMap[character.Key] =
+                        CoroutineHelper.Inst.StartCoroutine(DissolveOut(character.Key));
+            }
+        }
+        
+        private void DissolveIn(CharacterScreenPositionEnum screenPosition, 
+            float duration = GlobalConstant.ANIMATION_DISSOLVE_DURATION)
+        {
+            _characterUiViewMap[screenPosition].Image.color = GlobalConstant.ColorWitheClear;
+            _characterUiViewMap[screenPosition].Image.DOFade(1.0f, duration);
+        }
+        
+        private IEnumerator DissolveOut(CharacterScreenPositionEnum screenPosition, 
+            float duration = GlobalConstant.ANIMATION_DISSOLVE_DURATION)
+        {
+            _characterUiViewMap[screenPosition].Image.color = Color.white;
+            _characterUiViewMap[screenPosition].Image.DOFade(0.0f, duration);
+            
+            yield return new WaitForSeconds(duration);
+            
+            _characterUiViewMap[screenPosition].Visible = false;
+            _enabledMap[screenPosition] = false;
+            _characterUiViewMap[screenPosition].Image.sprite = null;
         }
     }
 }
--- Assets/Scripts/GameScene/Characters/CharacterUiView.cs
+++ Assets/Scripts/GameScene/Characters/CharacterUiView.cs
@@ -1,5 +1,4 @@
-﻿using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 namespace GameScene.Characters
@@ -8,37 +7,19 @@ namespace GameScene.Characters
     {
         [SerializeField] private Image image;
         [SerializeField] private CharacterScreenPositionEnum screenPosition;
+        public Image Image => image;
 
         public bool Visible
         {
             set => gameObject.SetActive(value);
         }
-
-        public Sprite Image
+        
+        public Sprite Sprite
         {
             set
             {
-                if (value == null)
-                {
-                    Visible = false;
-                    return;
-                }
-                
-                if(value == image.sprite)
-                {
-                    Visible = true;
-                    return;
-                }
-
-                Color color = new Color(1.0f, 1.0f, 1.0f, 0.0f);
-                
-                image.color = color;
                 image.sprite = value;
                 image.SetNativeSize();
-                
-                Dissolve();
-                
-                Visible = true;
             }
         }
 
@@ -46,11 +27,6 @@ namespace GameScene.Characters
         {
             get => transform.position;
             set => transform.position = value;
-        }
-
-        private void Dissolve(float duration = GlobalConstant.ANIMATION_DISSOLVE_DURATION)
-        {
-            image.DOFade(1.0f, duration);
         }
 
         public CharacterScreenPositionEnum ScreenPosition => screenPosition;
