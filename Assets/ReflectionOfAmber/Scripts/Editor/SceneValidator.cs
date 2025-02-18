47c1523578369d31183bae40afe3c88e16b6a864
dimag <dima.grach333@gmail.com>
2025-02-18 13:11:37 +0200

    scene validator

---
+++ Assets/ReflectionOfAmber/Scripts/Editor/SceneValidator.cs
@@ -0,0 +1,47 @@
+﻿using ReflectionOfAmber.Scripts.GameScene.ScreenPart;
+using ReflectionOfAmber.Scripts.GameScene.ScreenPart.SpecialSO;
+using UnityEditor;
+using UnityEngine;
+
+namespace ReflectionOfAmber.Scripts.Editor
+{
+    public class SceneValidator : EditorWindow
+    {
+        [MenuItem("ReflectionOfAmber/Validate scenes")]
+        public static void Validate()
+        {
+            ScreenSceneScriptableObject[] list = Resources.LoadAll<ScreenSceneScriptableObject>("Graphs");
+            
+            foreach (var screenScene in list)
+            {
+                
+                int index = 0;
+                foreach (var nextScene in screenScene.nextScenes)
+                {
+                    if (nextScene.Scene == null)
+                    {
+                        Debug.LogWarning($"[VALIDATOR] Scene: {screenScene.SceneKey} has missing scene in choose {index}");
+                    }
+
+                    if(nextScene.specialDependent.enable)
+                    {
+                        foreach (var special in nextScene.specialDependent.special)
+                        {
+                            if (special == null)
+                            {
+                                Debug.LogWarning($"[VALIDATOR] Scene: {screenScene.SceneKey} has missing special scene in choose {index}");
+                            }
+                            // switch (special.GetType())
+                            // {
+                            //     
+                            //         break;
+                            // }
+                        }
+                    }
+                    
+                    index++;
+                }
+            }
+        }
+    }
+}
