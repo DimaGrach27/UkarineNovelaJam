3885f79a4fa4a35402115635f25e32d3751419f9
dimag <dima.grach333@gmail.com>
2025-04-08 18:39:31 +0300

    add analytic keys chnager and profiles

---
+++ Assets/ReflectionOfAmber/Scripts/Editor/Debug_test/ChangeAnalyticGameKey.cs
@@ -0,0 +1,49 @@
+﻿#define TEST_KEY
+
+using UnityEditor;
+using UnityEngine;
+
+namespace ReflectionOfAmber.Scripts.Editor.Debug_test
+{
+    public class ChangeAnalyticGameKey
+    {
+#if TEST_KEY
+        private const string GameKey = "cddccd5a3bda77c6c7d8f2c559fe4223";
+        private const string SecretKey = "de3649a8c583bbe852521c6a2c6cca43fb490bdf";
+#else   
+        private const string GameKeyWin = "a54b960045b8944a04310bd2639d5f0c";
+        private const string SecretKeyWin = "6b337b9efcd317e8b489015ced0742d83a9862bd";
+#endif
+        
+        [MenuItem("ReflectionOfAmber/ChangeKeys")]
+        private static void ChangeAnalyticKeys()
+        {
+            GameAnalyticsSDK.Setup.Settings settings = Resources.Load<GameAnalyticsSDK.Setup.Settings>("GameAnalytics/Settings");
+#if UNITY_EDITOR_OSX
+            Debug.Log("Changing analytic keys in OSX");
+#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            Debug.Log("Changing analytic keys for WIN");
+#endif
+            settings.SetKeys(GameKey, SecretKey);
+            settings.NewVersion = PlayerSettings.bundleVersion;
+            
+            Debug.Log($"[GAME ANALYTICS] GameKey = {GameKey}");
+            Debug.Log($"[GAME ANALYTICS] GameSecret = {SecretKey}");
+            
+            int index = 0;
+            foreach (var plat in settings.Platforms)
+            {
+                Debug.Log($"[GAME ANALYTICS] Platform = {plat}");
+
+                Debug.Log($"[GAME ANALYTICS] GameKey = {settings.GetGameKey(index)}");
+                Debug.Log($"[GAME ANALYTICS] GameSecret = {settings.GetSecretKey(index)}");
+                
+                if (plat == RuntimePlatform.WindowsPlayer)
+                {
+                    GameAnalyticsSDK.Setup.Settings.UpdateKeys(index, GameKey, SecretKey);
+                }
+                index++;
+            }
+        }
+    }
+}
--- Assets/ReflectionOfAmber/Scripts/Editor/PreBuildKeysChooser.cs
+++ Assets/ReflectionOfAmber/Scripts/Editor/PreBuildKeysChooser.cs
@@ -1,3 +1,6 @@
+#define TEST_KEY
+
+using UnityEditor;
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
@@ -8,17 +11,68 @@ namespace ReflectionOfAmber.Scripts.Editor
     {
         public int callbackOrder => 0;
         
+#if GAME_DEMO && GAME_FINAL
+        private const string GameKeyWin = "a54b960045b8944a04310bd2639d5f0c";
+        private const string SecretKeyWin = "6b337b9efcd317e8b489015ced0742d83a9862bd";
+        
+        private const string GameKeyOSX = "c7630c3b5f6d95d352cd2a0abff87af1";
+        private const string SecretKeyOSX = "08ca02f66186abda3f39312f22456d540ae0f4da";
+#elif GAME_FINAL && !GAME_DEMO
+        private const string GameKeyWin = "b6af346853a69c1aafe4d09cd849d047";
+        private const string SecretKeyWin = "2932f438d66c6137e10ade0ecd532a9ab1d43592";
+        
+        private const string GameKeyOSX = "45e84645fd546dd10c4ac7c500deba9b";
+        private const string SecretKeyOSX = "1e867e0e96638460285bcd82e392de0e61016038";
+
+#else
+        private const string GameKeyWin = "cddccd5a3bda77c6c7d8f2c559fe4223";
+        private const string SecretKeyWin = "de3649a8c583bbe852521c6a2c6cca43fb490bdf";
+        
+        private const string GameKeyOSX = "b92bb7f27519cedeae17197483b4a357";
+        private const string SecretKeyOSX = "9bf51f6681794020c19d221fd9c998d63f8132ca";
+#endif
+        
         public void OnPreprocessBuild(BuildReport report)
         {
-            throw new System.NotImplementedException();
+            ChangeAnalyticKeys(report);
         }
 
         private void ChangeAnalyticKeys(BuildReport report)
         {
-            GameAnalyticsSDK.Setup.Settings settings = Resources.Load<GameAnalyticsSDK.Setup.Settings>("GameAnalytics");
+            GameAnalyticsSDK.Setup.Settings settings = Resources.Load<GameAnalyticsSDK.Setup.Settings>("GameAnalytics/Settings");
 #if UNITY_EDITOR_OSX
             Debug.Log("Changing analytic keys in OSX");
+#elif UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+            Debug.Log("Changing analytic keys for WIN");
 #endif
+            settings.NewVersion = PlayerSettings.bundleVersion;
+            
+            int index = 0;
+            foreach (var plat in settings.Platforms)
+            {
+                Debug.Log($"[GAME ANALYTICS] Platform = {plat}");
+
+                Debug.Log($"[GAME ANALYTICS] GameKey = {settings.GetGameKey(index)}");
+                Debug.Log($"[GAME ANALYTICS] GameSecret = {settings.GetSecretKey(index)}");
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+                if (plat == RuntimePlatform.WindowsPlayer)
+                {
+                    GameAnalyticsSDK.Setup.Settings.UpdateKeys(index, GameKeyWin, SecretKeyWin);
+                    Debug.Log($"[GAME ANALYTICS] GameKey = {GameKeyWin}");
+                    Debug.Log($"[GAME ANALYTICS] GameSecret = {SecretKeyWin}");
+                }
+#endif
+                
+#if UNITY_EDITOR_OSX
+                if (plat == RuntimePlatform.OSXPlayer)
+                {
+                    GameAnalyticsSDK.Setup.Settings.UpdateKeys(index, GameKeyOSX, SecretKeyOSX);
+                    Debug.Log($"[GAME ANALYTICS] GameKey = {GameKeyOSX}");
+                    Debug.Log($"[GAME ANALYTICS] GameSecret = {SecretKeyOSX}");
+                }
+#endif
+                index++;
+            }
         }
     }
 }
